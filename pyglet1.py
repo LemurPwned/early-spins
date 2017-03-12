443d532a64acdf06d20141989028a411b775ddd9
pawelkulig1 <pawelkulig1@gmail.com>
2017-03-12 09:20:31 +0100

    pyglet animation 3D model added

---
+++ pyglet1.py
@@ -0,0 +1,71 @@
+import pyglet
+from pyglet.gl import *
+from pyglet.window import key
+from OpenGL.GLUT import *
+
+WINDOW   = 800
+INCREMENT = 5
+
+class Window(pyglet.window.Window):
+
+   # Cube 3D start rotation
+   xRotation = yRotation = 30  
+
+   def __init__(self, width, height, title=''):
+       super(Window, self).__init__(width, height, title)
+       glClearColor(0, 0, 0, 1)
+       glEnable(GL_DEPTH_TEST)  
+
+   def on_draw(self):
+       # Clear the current GL Window
+       self.clear()
+
+       # Push Matrix onto stack
+       glPushMatrix()
+
+       glRotatef(self.xRotation, 1, 0, 0)
+       glRotatef(self.yRotation, 0, 1, 0)
+
+       # Draw the six sides of the cube
+       glBegin(GL_LINES)
+       
+       for i in range(-30,30):
+           glColor3ub(i*2, 150, 100)
+           for j in range(-30, 30):
+               glVertex3f(i*5,j*5,0)
+               glVertex3f(i*5,j*5,5)
+       glEnd()
+
+       # Pop Matrix off stack
+       glPopMatrix()
+
+   def on_resize(self, width, height):
+       # set the Viewport
+       glViewport(0, 0, width, height)
+
+       # using Projection mode
+       glMatrixMode(GL_PROJECTION)
+       glLoadIdentity()
+
+       aspectRatio = width / height
+       gluPerspective(85, aspectRatio, 1, 1000)
+
+       glMatrixMode(GL_MODELVIEW)
+       glLoadIdentity()
+       glTranslatef(0, 0, -400)
+
+
+   def on_text_motion(self, motion):
+       if motion == key.UP:
+            self.xRotation -= INCREMENT
+       elif motion == key.DOWN:
+           self.xRotation += INCREMENT
+       elif motion == key.LEFT:
+           self.yRotation -= INCREMENT
+       elif motion == key.RIGHT:
+           self.yRotation += INCREMENT
+
+            
+if __name__ == '__main__':
+   Window(WINDOW, WINDOW, 'Pyglet Colored Cube')
+   pyglet.app.run()
