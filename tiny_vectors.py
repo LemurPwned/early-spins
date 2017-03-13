26deff94b3b44bfc0a4790f3caed16072756f305
Jakub Mojsiejuk <jakub.mojsiejuk@gmail.com>
2017-03-13 15:51:45 +0100

    Update on color function

--- tiny_vectors.py
+++ tiny_vectors.py
@@ -10,25 +10,34 @@ class Vector:
     def set_color(self, color):
         self.color = color
 
-
-
 def dot(v1,v2):
         return v1.x*v2.x+v1.y*v2.y+v1.z*v2.z
 
 def relative_direction(v1, v2=Vector(1,0,0)):
         return np.arccos(dot(v1,v2)/(v1.norm*v2.norm))
 
+def color_mapping(angle1, angle2, angle3, scale=1):
+        #r - v1, g - v2, b - v3
+        if angle1 > 0:
+            r = np.ceil(np.array(angle1*255/scale)/np.pi)
+        else:
+            r = 0
+        if angle2 > 0:
+            g = np.ceil(np.array(angle2*255/scale)/np.pi)
+        else:
+            g = 0
+        if angle3 > 0:
+            b = np.ceil(np.array(angle3*255/scale)/np.pi)
+        else:
+            b = 0
+        return (r,g,b)
+
 def color_map(angle):
         # red is (255,0,0); yellow is (255,255,0) will turn yellow->red
         if angle<0:
-            return (0,255,0)
+            return 0
         else:
             new_map = np.ceil(np.array(angle*255)/np.pi)
-            return (255,255-new_map,0)
+            return new_map
 
 #test
-'''
-set_of_angles =  np.linspace(0,np.pi,10)
-for angle in set_of_angles:
-    print(color_map(angle))
-'''
