ebf2555c8595e4db3b6e15e1b39bff44cd3be10b
Jakub Mj <jakub.mojsiejuk@gmail.com>
2017-04-14 18:22:36 +0200

    Fixed animations in 3D and reformatted color. Color series in 3D need farther improvements

--- input_parser.py
+++ input_parser.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import tiny_vectors as vc
-import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import cm
+
 
 def extract_base_data(filename):
     base_data = {}
@@ -16,16 +16,17 @@ def extract_base_data(filename):
             if ':' in g:
                 x = g.index(':')
                 if g[2:x] in base_data:
-                    base_data[g[2:x]] += g[x+1:-1]
+                    base_data[g[2:x]] += g[x + 1:-1]
                 else:
-                    base_data[g[2:x]] = g[x+1:-1]
+                    base_data[g[2:x]] = g[x + 1:-1]
                 try:
-                    #print(float(base_data[g[2:x]]))
+                    # print(float(base_data[g[2:x]]))
                     base_data[g[2:x]] = float(base_data[g[2:x]])
                 except:
                     pass
     f.close()
     return base_data, count
+
 
 def read_header_file(filename):
     with open(filename, 'r') as f:
@@ -46,49 +47,53 @@ def read_header_file(filename):
                 pass
         new_cont.append(temp_line)
     col = ['Total energy', 'Energy calc count', 'Max dm/dt', 'dE/dt',
-        'Delta E', '_MRmagnetoresistance', 'Energy ', 'Max Spin Ang',
-        'Stage Max Spin Ang', 'Run Max Spin Ang', '_DemagEnergy',
-        '_MREnergy', '_TwoSurfaceExchangeFFEnergy',
-        '_UniaxialAnisotropystatEnergy', '_UniaxialAnisotropyipEnergy ',
-        '_UniaxialAnisotropydynEnergy', '_UZeemanEnergy', '_UZeemanB',
-        '_UZeemanBx', '_UZeemanBy', '_UZeemanBz', 'Iteration', 'Stage iteration',
-        'Stage ', 'mx', 'my', 'mz', 'Last time step', 'Simulation time']
+           'Delta E', '_MRmagnetoresistance', 'Energy ', 'Max Spin Ang',
+           'Stage Max Spin Ang', 'Run Max Spin Ang', '_DemagEnergy',
+           '_MREnergy', '_TwoSurfaceExchangeFFEnergy',
+           '_UniaxialAnisotropystatEnergy', '_UniaxialAnisotropyipEnergy ',
+           '_UniaxialAnisotropydynEnergy', '_UZeemanEnergy', '_UZeemanB',
+           '_UZeemanBx', '_UZeemanBy', '_UZeemanBz', 'Iteration', 'Stage iteration',
+           'Stage ', 'mx', 'my', 'mz', 'Last time step', 'Simulation time']
 
     df = pd.DataFrame.from_records(new_cont, columns=col)
     return df
 
-def form_dataframe(filename, to_skip, cols=['Whitespace', 'x','y','z']):
+
+def form_dataframe(filename, to_skip, cols=['Whitespace', 'x', 'y', 'z']):
     data = pd.read_csv(filename, delimiter=' ', skiprows=to_skip)
     if cols != None:
         data.columns = cols
         data.drop('Whitespace', axis=1, inplace=True)
-        data.drop([data.shape[0]-2,data.shape[0]-1],axis=0, inplace=True)
-        data[['x','y','z']] = data[['x','y','z']].astype(float)
+        data.drop([data.shape[0] - 2, data.shape[0] - 1], axis=0, inplace=True)
+        data[['x', 'y', 'z']] = data[['x', 'y', 'z']].astype(float)
     return data
+
 
 def calculate_color(data, relate):
     '''
     calculates the color of each vector
     '''
-    norms = pd.Series(data['x']**2+data['y']**2+data['z']**2, dtype=np.float).apply(np.sqrt)
-    dot = pd.Series(data['x']*relate.x+data['y']*relate.y+data['z']*relate.z, dtype=np.float)
-    angle = pd.Series(dot/(relate.norm*norms), dtype=np.float).apply(np.arccos).fillna(np.float(-1))
+    norms = pd.Series(data['x'] ** 2 + data['y'] ** 2 + data['z'] ** 2, dtype=np.float).apply(np.sqrt)
+    dot = pd.Series(data['x'] * relate.x + data['y'] * relate.y + data['z'] * relate.z, dtype=np.float)
+    angle = pd.Series(dot / (relate.norm * norms), dtype=np.float).apply(np.arccos).fillna(np.float(-1))
     color = pd.Series(angle, dtype=tuple).apply(vc.color_map)
     return color
+
 
 def layer_splitter(data, base_data):
     '''
     splits data into n separate layers with proper indices
     '''
-    (x,y,z) = (int(base_data['xnodes']),int(base_data['ynodes']),int(base_data['znodes']))
-    thickness = x*y
-    layers = [data.iloc[thickness*i:thickness*i + thickness , :] for i in range(z)]
+    (x, y, z) = (int(base_data['xnodes']), int(base_data['ynodes']), int(base_data['znodes']))
+    thickness = x * y
+    layers = [data.iloc[thickness * i:thickness * i + thickness, :] for i in range(z)]
     return layers
+
 
 def plotters(data):
     cmap = sns.cubehelix_palette(as_cmap=True, dark=0, light=1, reverse=True)
-    #sns.kdeplot(data['Total energy'], cut=0, bw=0.2);
-    sns.jointplot(data['mx'],data['Total energy'], kind="kde")
+    # sns.kdeplot(data['Total energy'], cut=0, bw=0.2);
+    sns.jointplot(data['mx'], data['Total energy'], kind="kde")
     plt.show()
 
 
@@ -98,15 +103,16 @@ def color2d(data, base_vectors):
     '''
     angles = []
     sens = []
-    norms = pd.Series(data['x']**2+data['y']**2+data['z']**2, dtype=np.float).apply(np.sqrt)
+    norms = pd.Series(data['x'] ** 2 + data['y'] ** 2 + data['z'] ** 2, dtype=np.float).apply(np.sqrt)
     for related_vec in base_vectors:
-        dot = pd.Series(data['x']*related_vec.x+data['y']*related_vec.y+data['z']*related_vec.z, dtype=np.float)
-        angle = pd.Series(dot/(related_vec.norm*norms), dtype=np.float).apply(np.arccos).fillna(np.float(0))
+        dot = pd.Series(data['x'] * related_vec.x + data['y'] * related_vec.y + data['z'] * related_vec.z,
+                        dtype=np.float)
+        angle = pd.Series(dot / (related_vec.norm * norms), dtype=np.float).apply(np.arccos).fillna(np.float(0))
         angles.append(angle)
-        sensitive = pd.Series(np.power(angle,25))
+        sensitive = pd.Series(np.power(angle, 25))
         sens.append(sensitive)
-    #sns.jointplot(data['x'], data['y'], hue=angles[0], kind="hex")
-    plt.scatter(data['x'],data['y'], c=sens[0], cmap=cm.jet)
+    # sns.jointplot(data['x'], data['y'], hue=angles[0], kind="hex")
+    plt.scatter(data['x'], data['y'], c=sens[0], cmap=cm.jet)
     plt.show()
     print(np.max(angles[0]), np.max(sens[0]))
     print(np.min(angles[0]), np.min(sens[0]))
@@ -114,37 +120,11 @@ def color2d(data, base_vectors):
     print(np.mean(sens[0]), np.median(sens[0]), np.std(sens[0]))
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     filename = './data/voltage-spin-diode-Oxs_TimeDriver-Magnetization-00-0000000.omf'
     filename2 = './data/voltage-spin-diode.odt'
     base_data, count = extract_base_data(filename)
     print(base_data, count)
-    to_skip=[x for x in range(count)]
+    to_skip = [x for x in range(count)]
     data = form_dataframe(filename, to_skip)
     print(data)
-
-    # relate = vc.Vector(1, 0, 0)
-    # color = calculate_color(data, relate)
-    # #print(base_data)
-    #
-    # v1 = vc.Vector(1,0,0)
-    # v2 = vc.Vector(0,1,0)
-    # v3 = vc.Vector(0,0,1)
-    #
-    # layers = layer_splitter(data, base_data)
-    # #color2d(layers[0], [v1, v2, v3])
-    # data[['x','y','z']] = data[['x','y','z']]/np.max(np.abs(data[['x','y','z']]))
-    # print(np.max(np.abs(data)))
-    # #print(data.columns.values.tolist())
-    # #print(data.shape[0])
-    #
-    # df = read_header_file(filename2)
-    # #print(df.columns.values.tolist(), df.shape)
-    # #print(df['Iteration'].head())
-    # #plotters(df)
-    #
-    # vc.df_color(data, v1, v2, v3)
-    # #node_iterator(base_data, print)
-    # #print(df.head())
--- tiny_vectors.py
+++ tiny_vectors.py
@@ -1,12 +1,12 @@
 import numpy as np
-import pandas as pd
+
 
 class Vector:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-        self.norm = np.sqrt(self.x**2 + self.y**2 + self.z**2)
+        self.norm = np.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
     def __str__(self):
         return "{}, {}, {}".format(self.x, self.y, self.z)
@@ -15,35 +15,38 @@ class Vector:
         return "{}, {}, {}".format(self.x, self.y, self.z)
 
 
-def dot(v1,v2):
-        return v1.x*v2.x+v1.y*v2.y+v1.z*v2.z
+def dot(v1, v2):
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
 
-def relative_direction(v1, v2=Vector(1,0,0)):
-        return np.arccos(dot(v1,v2)/(v1.norm*v2.norm))
+
+def relative_direction(v1, v2=Vector(1, 0, 0)):
+    return np.arccos(dot(v1, v2) / (v1.norm * v2.norm))
+
 
 def color_map(angle):
-    return np.ceil(np.array(angle*255)/np.pi)
+    return np.ceil(np.array(angle * 255) / np.pi)
+
 
 def rescale(value, max_val, max_rescaled):
-    return (value*max_rescaled)/max_val
+    return (value * max_rescaled) / max_val
 
 
-#UNIT TEST xD
+# UNIT TEST xD
 def color_test():
-    v1 = Vector(1,0,0)
-    v2 = [Vector(np.random.randint(0,30),np.random.randint(0,30),np.random.randint(0,30)) for z in range(0,300)]
+    v1 = Vector(1, 0, 0)
+    v2 = [Vector(np.random.randint(0, 30), np.random.randint(0, 30), np.random.randint(0, 30)) for z in range(0, 300)]
     dot1 = []
     dot2 = []
     k = 7
     max_vals = []
     for vec in v2:
-        max_vals.append(color_map(relative_direction(v1,vec)**k))
+        max_vals.append(color_map(relative_direction(v1, vec) ** k))
     max_val = np.max(max_vals)
     print(np.max(max_vals))
     print(max_val)
     for vec in v2:
-        d1 = color_map(relative_direction(v1,vec))
-        d2 = color_map(rescale(relative_direction(v1,vec)**k,max_val,255))
+        d1 = color_map(relative_direction(v1, vec))
+        d2 = color_map(rescale(relative_direction(v1, vec) ** k, max_val, 255))
         print("Standard dot {} ".format(d1))
         print("Sensitive dot {} ".format(d2))
         print("\n")
@@ -55,4 +58,4 @@ def color_test():
     print(np.median(dot1), np.mean(dot1), np.std(dot1))
     print(np.median(dot2), np.mean(dot2), np.std(dot2))
 
-#color_test()
+# color_test()
