686a1db1c2872590c43bcd4c58f42c0312725d38
pawelkulig1 <pawelkulig1@gmail.com>
2017-10-20 11:26:31 +0200

    canvas plot added

--- MAIN2/main.py
+++ MAIN2/main.py
@@ -6,13 +6,14 @@ from MainWindowTemplate import Ui_MainWindow
 
 #from mainWindow import GLWidget, Helper
 from structureDrawer import DrawData
+from pyqtMatplotlib import PlotCanvas
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.setWindowTitle("ESE - Early Spins Enviroment")
-
+        self.setGeometry(10,10,1280, 768)
         self.openGLWidget.initializeGL()
         self.openGLWidget.resizeGL(800, 600)
         self.canvas = DrawData()
@@ -23,10 +24,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         timer = QTimer(self)
         timer.timeout.connect(self.openGLWidget.update)
         timer.start(0)
+
         self.addButtons()
+        self.events()
 
     def events(self):
-        self.actionLoad_Directory.clicked.connect()
+        #FILE SUBMENU
+        #self.actionLoad_File.clicked.connect()
+        #self.actionLoad_Directory.clicked.connect()
+
+        #EDIT SUBMENU
+        #self.actionPlot.clicked.connect()
+        #self.actionAnimation.clicked.connect()
+
+        #VIEW SUBMENU
+        self.action2_Window_Grid.triggered.connect(self.make2WindowsGrid)
+        #self.action4_Windows_Grid.clicked.connect()
+
+
+    def make2WindowsGrid(self):
+        middlePos = (self.width())/2
+        self.openGLWidget.setGeometry(self.openGLWidget.pos().x(), self.openGLWidget.pos().y(), middlePos-50, self.height())
+
+        #create matplotlib window
+        self.canvasPlot1 = PlotCanvas(self, width=5, height=5)
+        self.canvasPlot1.move(middlePos+50, self.openGLWidget.pos().y())
+        self.canvasPlot1.resize(100, 100)
+
+        #width of the other widget
 
 
     def addButtons(self):
